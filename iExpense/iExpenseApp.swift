178fe9373567c405ec5df483f8b28a26f93fd0b9
Blair Duddy <blairduddy@hotmail.co.uk>
2023-06-06 20:54:37 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Blair Duddy on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
